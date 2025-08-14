3eb39ca2bfd06b4ed8709ceef3bf066149960c88
unknown <josimarmoraaguirre2002.jma16@gmail.com>
2025-08-14 11:25:27 -0600

    Mi primer commit en Java

---
+++ IF2000_IntroProgra/src/main/java/com/mycompany/if2000_introprogra/IF2000_IntroProgra.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.if2000_introprogra;
+
+/**
+ *
+ * @author estudiante
+ */
+public class IF2000_IntroProgra {
+
+    public static void main(String[] args) {
+       
+        int age = 22;
+        String name = " Josimar ";
+        
+        System.out.println("Hello World ");
+        System.out.println("Name " + name);
+        System.out.println("age " + age);
+    }
+}
